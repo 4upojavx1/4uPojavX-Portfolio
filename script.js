ebc783912c906f65ef50e46959d5494f57e97586
4upojavx1 <4upojavx1@gmail.com>
2026-08-29 22:18:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// ================================================
+// 4uPojavX PORTFOLIO
+// ================================================
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("4uPojavX Portfolio Loaded");
+
+});
